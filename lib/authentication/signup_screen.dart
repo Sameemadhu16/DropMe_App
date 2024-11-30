5399f1db9edb208217c0204d9205644f1382b424
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-11-30 23:13:16 +0530

    Add input text fields for the sign up page

--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -8,20 +8,102 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+
+  TextEditingController usernameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Sign Up'),
+        title: const Text('Sign Up'),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
-            children: [],
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+              ),
+              const Text(
+                "Create a User\'s Account",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              //text form field
+              Padding(
+                padding: EdgeInsets.all(22),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: usernameTextEditingController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "Username",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                        hintText: "Enter your Username",
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        ),
+                      ),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    TextField(
+                        controller: emailTextEditingController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(
+                            fontSize: 14,
+                          ),
+                          hintText: "Enter your Email",
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        )),
+                   
+                    TextField(
+                        controller: passwordTextEditingController,
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                            fontSize: 14,
+                          ),
+                          hintText: "Enter your Password",
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        )),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-        
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'authentication/signup_screen.dart'; // Import the SignUpScreen
+import 'authentication/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return SafeArea(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Colors.black,
+        ),
+        home: HomeScreen(),
+        routes: {
+          '/signup': (context) =>
+              const SignUpScreen(), // Add SignUpScreen route
+          '/login': (context) => const LoginScreen(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: const HomeScreen(), // Set the initial screen
-      routes: {
-        '/signup': (context) => const SignUpScreen(), // Define the route
-      },
     );
   }
 }
