d8799b4c26eaf5079a6b2f95767e99d1b57cfcfa
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2024-12-08 13:27:40 +0530

    add update map theme styles

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber_app_clone/global/global_var.dart';
 
@@ -16,6 +19,23 @@ class _HomePageState extends State<HomePage> {
       Completer<GoogleMapController>();
   GoogleMapController? controllerGoogleMap;
 
+  void updateMapTheme(GoogleMapController controller) {
+    getJsonFileFromThemes("themes/night_style.json")
+        .then((value) => setGoogleMapStyle(value, controller));
+  }
+
+  Future<String> getJsonFileFromThemes(String mapStylePath) async {
+    ByteData byteDate = await rootBundle.load(mapStylePath);
+    var list = byteDate.buffer
+        .asUint8List(byteDate.offsetInBytes, byteDate.lengthInBytes);
+    return utf8.decode(list);
+  }
+
+  setGoogleMapStyle(
+      String googleMapStyle, GoogleMapController controller) async {
+    controller.setMapStyle(googleMapStyle);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,12 +45,11 @@ class _HomePageState extends State<HomePage> {
               mapType: MapType.normal,
               myLocationButtonEnabled: true,
               initialCameraPosition: googlePlexInitialPosition,
-              onMapCreated: (GoogleMapController mapController)
-              {
+              onMapCreated: (GoogleMapController mapController) {
                 controllerGoogleMap = mapController;
+                updateMapTheme(controllerGoogleMap!);//use of null assertion operator100% sure that controllerGoogleMap is not null at this point
                 googleMapCompleterController.complete(controllerGoogleMap);
-              }
-              )
+              }),
         ],
       ),
     );
